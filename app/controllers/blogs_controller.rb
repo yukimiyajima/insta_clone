ac183f4f3d67697c15a9efb922326d9073077aec
yuki miyajima <miyajima383838@gmail.com>
2020-04-16 16:56:18 +0900

    提出前エラー修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
-  before_action :ensure_correct_user, {only: [:show, :edit, :update]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
 
   def index
     @blogs = Blog.all
